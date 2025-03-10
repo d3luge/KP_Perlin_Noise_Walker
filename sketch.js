745b7f7d413ac88ab9e7df1b21df7b66e48f09b6
d3luge <olejarjuraj0@gmail.com>
2025-03-10 09:42:41 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.xoff = 0; // Random step: -1, 0, or 1
+    this.yoff = 1000;
+  }
+
+  move() {
+    
+    this.x = noise(this.xoff)*width;
+    //console.log(this.x);
+    this.y = noise(this.yoff)*height;
+    
+    this.xoff += 0.005;
+    this.yoff += 0.005;
+  }
+
+  show() {
+    noFill(0);
+    stroke(0, 25);
+    ellipse(this.x, this.y, 8, 8);
+  }
+}
+
+let walker;
+
+function setup() {
+  createCanvas(400, 400);
+  walker = new Walker();
+  background(255);
+}
+
+function draw() {
+  walker.move();
+  //background(255)
+  walker.show();
+}
